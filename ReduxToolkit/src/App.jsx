6f5df578357077e0edea6850d6ab37e8dbdcebae
vijay brat pandey <vijaybratpandey.com>
2024-02-26 19:08:14 +0530

    RaduxToolkit

---
+++ ReduxToolkit/src/App.jsx
@@ -0,0 +1,14 @@
+import AddTodo from "../src/components/AddTodo"
+import Todos from "./components/Todos"
+
+function App() {
+  return (
+    <>
+       <h1>Reduxtoolkit</h1>
+       <AddTodo/>
+       <Todos/>
+    </>
+  )
+}
+
+export default App
